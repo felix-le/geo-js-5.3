50f0ba835d998f815b259c5a8de984fa25409e2d
Viet Anh Le <mc.vietanh@gmail.com>
2021-10-24 09:36:06 -0400

    update be

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,35 @@
+let votesArr = [0, 0, 0, 0];
+
+function countVotes(vote) {
+  switch (vote) {
+    // All cases are for the votesArr
+    case 'A':
+      votesArr[0]++;
+      break;
+    case 'B':
+      votesArr[1]++;
+      break;
+    case 'C':
+      votesArr[2]++;
+      break;
+    case 'D':
+      votesArr[3]++;
+      break;
+    case 'R':
+      votesArr = [0, 0, 0, 0];
+      break;
+    default:
+      break;
+  }
+}
+
 exports.handler = async (event) => {
   const v = event.queryStringParameters.vote;
+  countVotes(v);
 
   return {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(v),
+    body: JSON.stringify(votesArr),
   };
 };
--- poll_site/index.js
+++ poll_site/index.js
@@ -11,31 +11,6 @@ const options = {
   },
 };
 
-let votesArr = [0, 0, 0, 0];
-
-function countVotes(vote) {
-  switch (vote) {
-    // All cases are for the votesArr
-    case 'A':
-      votesArr[0]++;
-      break;
-    case 'B':
-      votesArr[1]++;
-      break;
-    case 'C':
-      votesArr[2]++;
-      break;
-    case 'D':
-      votesArr[3]++;
-      break;
-    case 'R':
-      votesArr = [0, 0, 0, 0];
-      break;
-    default:
-      break;
-  }
-}
-
 const ctx = document.querySelector('#myChart').getContext('2d');
 const chart = new Chart(ctx, {
   type: 'bar',
